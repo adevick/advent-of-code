edb4697e94d435722d0a26929f07e3fa9c88e6a4
adevick <anthony.devick.ext@bayer.com>
2022-12-02 10:34:09 -0600

    Day one complete!

--- 2022-day-1/index.js
+++ 2022-day-1/index.js
@@ -2,27 +2,23 @@ const { readFileSync, promises: fsPromises } = require('fs');
 
 function syncReadFile(filename) {
   const contents = readFileSync(filename, 'utf-8');
-
   const arr = contents.split(/\r?\n/);
-
-  // console.log(arr); // 👉️ ['One', 'Two', 'Three', 'Four']
-
   return arr;
 }
 
 console.log('hello world');
 console.log('Reading input.txt');
 const input = syncReadFile('./input.txt');
-let elveNumber = 0;
+let elevNumber = 0;
 let totalCalories = 0;
 let elves = [];
 input.forEach(x => {
   if (x !== '') {
     totalCalories += Number(x);
   } else {
-    elveNumber += 1;
+    elevNumber += 1;
     elves.push({
-      elveNumber: elveNumber,
+      elevNumber: elevNumber,
       totalCalories: Number(totalCalories)
     })
     totalCalories = 0;
@@ -31,6 +27,17 @@ input.forEach(x => {
 
 const maxCalories = Math.max(...(elves.map((x) => x.totalCalories)));
 
-console.log(elves.find((x) => x.totalCalories === maxCalories));
+function findMaxCalories(elves) {
+  return Math.max(...(elves.map((x) => x.totalCalories)));
+}
 
+const topElev1 = elves.find((x) => x.totalCalories === findMaxCalories(elves));
+elves.splice(topElev1.elevNumber - 1, 1);
+const topElev2 = elves.find((x) => x.totalCalories === findMaxCalories(elves));
+elves.splice(topElev2.elevNumber - 1, 1);
+const topElev3 = elves.find((x) => x.totalCalories === findMaxCalories(elves));
+
+const totalCaloriesOfTop3 = topElev1.totalCalories + topElev2.totalCalories + topElev3.totalCalories;
+console.log('MostCalories: ', topElev1);
+console.log('total of top 3 elves: ', totalCaloriesOfTop3);
 
