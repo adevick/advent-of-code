7ced7910f57cb75f5b515c2d80412aff98a104e1
adevick <anthony.devick.ext@bayer.com>
2022-12-02 11:25:17 -0600

    finished part 1 day 2

--- 2022-day-2/index.js
+++ 2022-day-2/index.js
@@ -1,1 +1,65 @@
 console.log('hello world');
+
+const { readFileSync, promises: fsPromises } = require('fs');
+
+function syncReadFile(filename) {
+  const contents = readFileSync(filename, 'utf-8');
+  const arr = contents.split(/\r?\n/);
+  return arr;
+}
+
+const strategyGuide = syncReadFile('./strategyGuide.txt');
+
+const scoreOfShapes = {
+  Rock: 1,
+  Paper: 2,
+  Scissors: 3
+}
+
+const scoreOfOutcome = {
+  lost: 0,
+  draw: 3,
+  win: 6
+}
+
+function whoPlayedWhat(value) {
+  if (value === 'A' || value === 'X') {
+    return scoreOfShapes.Rock;
+  }
+  if (value === 'B' || value === 'Y') {
+    return scoreOfShapes.Paper;
+  }
+  if (value === 'C' || value === 'Z') {
+    return scoreOfShapes.Scissors;
+  }
+}
+
+function didIWin(elevPlayed, iPlayed) {
+  if (elevPlayed === scoreOfShapes.Rock && iPlayed === scoreOfShapes.Scissors ||
+    elevPlayed === scoreOfShapes.Scissors && iPlayed === scoreOfShapes.Paper ||
+    elevPlayed === scoreOfShapes.Paper && iPlayed === scoreOfShapes.Rock) {
+    return scoreOfOutcome.lost;
+  }
+  if (iPlayed === scoreOfShapes.Rock && elevPlayed === scoreOfShapes.Scissors ||
+    iPlayed === scoreOfShapes.Scissors && elevPlayed === scoreOfShapes.Paper ||
+    iPlayed === scoreOfShapes.Paper && elevPlayed === scoreOfShapes.Rock) {
+    return scoreOfOutcome.win;
+  }
+  if (elevPlayed === iPlayed) {
+    return scoreOfOutcome.draw;
+  }
+}
+
+console.log(strategyGuide);
+let totalScore = 0;
+strategyGuide.forEach(game => {
+  const elevPlayed = game.substring(0, 1);
+  const iPlayed = game.substring(2, 3);
+
+  const elvesValue = whoPlayedWhat(elevPlayed);
+  const iPlayedValue = whoPlayedWhat(iPlayed);
+  const winningScore = didIWin(elvesValue, iPlayedValue);
+  totalScore += (winningScore + iPlayedValue);
+});
+
+console.log('totalScore:', totalScore);
