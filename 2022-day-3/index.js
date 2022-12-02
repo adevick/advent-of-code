8fe12f04d1ca7462bafd9764b50fa1fa4571a97c
adevick <anthony.devick.ext@bayer.com>
2022-12-02 13:50:30 -0600

    read input

--- 2022-day-3/index.js
+++ 2022-day-3/index.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+const { readFileSync, promises: fsPromises } = require('fs');
+
+function syncReadFile(filename) {
+  const contents = readFileSync(filename, 'utf-8');
+  const arr = contents.split(/\r?\n/);
+  return arr;
+}
+
+const data = syncReadFile('./input.txt');
+
+console.log('Hello', data);
