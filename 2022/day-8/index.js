1465b5c632760233e9381b03a0b38eb9118648a1
adevick <anthony.devick.ext@bayer.com>
2022-12-16 22:26:30 -0600

    day 8 part 2

--- 2022/day-8/index.js
+++ 2022/day-8/index.js
@@ -8,7 +8,7 @@ function syncReadFile(filename) {
 
 const treeRows = syncReadFile('./input.txt');
 
-console.log('trees', treeRows);
+// console.log('trees', treeRows);
 
 function visibleFromLeftToRight(rowOfTrees, maxTreeHightForRow, forward) {
   let tallestTreeSoFar = 0;
@@ -71,6 +71,7 @@ let x = 0;
 let y = 0;
 let treesThatCanBeeSeen = [];
 let topToBottomTrees = [];
+let treeGrid = [];
 
 for (let i = 0; i < treeRows.length; i++) {
   const row = treeRows[i];
@@ -101,9 +102,104 @@ for (let i = 0; i < topToBottomTrees.length; i++) {
   visibleTreesByColumns(topTopBottom, maxTreeHightForRow, true);
 
   const bottomToTop = column.split('').reverse();
-  visibleTreesByColumns(bottomToTop, maxTreeHightForRow, false); 
+  visibleTreesByColumns(bottomToTop, maxTreeHightForRow, false);
 }
 
-console.log('Trees that can bee seen: ', treesThatCanBeeSeen);
+x = 0
+y = 0
+const gridHeight = treeRows.length;
+const gridWidth = treeRows[0].split('').length;
+for (let i = 0; i < treeRows.length; i++) {
+
+  const row = treeRows[i];
+  y = i;
+  const rowLeftToRight = row.split('');
+  for (let index = 0; index < rowLeftToRight.length; index++) {
+    x = index;
+    const tree = rowLeftToRight[index];
+    treeGrid.push({ x: x, y: y, treeHeight: Number(tree), up: 0, down: 0, left: 0, right: 0 })
+  }
+}
+
+
+let bestScenicScore = 0;
+treeGrid.forEach(tree => {
+  tree.up = getUpScore(tree);
+  tree.down = getDownScore(tree);
+  tree.left = getLeftScore(tree);
+  tree.right = getRightScore(tree);
+  const scenicScore = (tree.up * tree.down * tree.left * tree.right);
+  if (bestScenicScore < scenicScore) {
+    bestScenicScore = scenicScore;
+  }
+});
+
+function getUpScore(tree) {
+  let upScore = 0;
+  for (let index = tree.y - 1; index >= 0; index--) {
+    const northTree = treeGrid.find((someTree) => someTree.x === tree.x && someTree.y === index);
+    if (northTree.treeHeight < tree.treeHeight) {
+      upScore += 1;
+    } else {
+      if (northTree.treeHeight >= tree.treeHeight) {
+        upScore += 1;
+        break;
+      }
+    }
+  }
+  return upScore;
+}
+
+function getDownScore(tree) {
+  let downScore = 0;
+  for (let index = tree.y + 1; index < gridHeight; index++) {
+    const southTree = treeGrid.find((someTree) => someTree.x === tree.x && someTree.y === index);
+    if (southTree.treeHeight < tree.treeHeight) {
+      downScore += 1;
+    } else {
+      if (southTree.treeHeight >= tree.treeHeight) {
+        downScore += 1;
+        break;
+      }
+    }
+  }
+  return downScore;
+}
+
+function getLeftScore(tree) {
+  let leftScore = 0;
+  for (let index = tree.x - 1; index >= 0; index--) {
+    const westTree = treeGrid.find((someTree) => someTree.x === index && someTree.y === tree.y);
+    if (westTree.treeHeight < tree.treeHeight) {
+      leftScore += 1;
+    } else {
+      if (westTree.treeHeight >= tree.treeHeight) {
+        leftScore += 1;
+        break;
+      }
+    }
+  }
+  return leftScore;
+}
+
+function getRightScore(tree) {
+  let rightScore = 0;
+  for (let index = tree.x + 1; index < gridWidth; index++) {
+    const eastTree = treeGrid.find((someTree) => someTree.x === index && someTree.y === tree.y);
+    if (eastTree.treeHeight < tree.treeHeight) {
+      rightScore += 1;
+    } else {
+      if (eastTree.treeHeight >= tree.treeHeight) {
+        rightScore += 1;
+        break;
+      }
+    }
+  }
+  return rightScore;
+}
+
+// console.log('Trees that can bee seen: ', treesThatCanBeeSeen);
 console.log('Total Visible trees: ', treesThatCanBeeSeen.length);
+console.log('The Best Scenic Score is: ',bestScenicScore);
+
 
