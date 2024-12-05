303aa87617b9da4295733eb8d5d51abf2ed06ba4
adevick <anthony.devick.ext@bayer.com>
2024-12-05 11:49:30 -0600

    part 2

--- 2024/day-3/code.js
+++ 2024/day-3/code.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const allContents = fs.readFileSync('puzzleInput.txt', 'utf-8');
-const regex = /mul\((?:[1-9][0-9]{0,2}|0),(?:[1-9][0-9]{0,2}|0)\)/g;
+const mulRegex = /mul\((?:[1-9][0-9]{0,2}|0),(?:[1-9][0-9]{0,2}|0)\)/g;
 
-const matches = allContents.match(regex);
+const matches = allContents.match(mulRegex);
 
 const multiply = (instruction) => {
     const regex = /mul\((\d{1,3}),(\d{1,3})\)/g;
@@ -18,6 +18,44 @@ const multiply = (instruction) => {
     }
 }
 
+function findMatchesInOrder(str) {
+  const mulRegexNonGlobal = /mul\((?:[1-9][0-9]{0,2}|0),(?:[1-9][0-9]{0,2}|0)\)/;
+  const doRegex = /do\(\)/;
+  const doNotRegex = /don't\(\)/;
+
+  const combinedRegex = new RegExp(`${mulRegexNonGlobal.source}|${doRegex.source}|${doNotRegex.source}`, 'g');
+  const matches = [];
+  let match;
+
+  while ((match = combinedRegex.exec(str)) !== null) {
+    if (mulRegexNonGlobal.test(match[0])) {
+      matches.push({ match: match[0], regex: 'mulRegex', index: match.index });
+    } else if (doRegex.test(match[0])) {
+      matches.push({ match: match[0], regex: 'doRegex', index: match.index });
+    } else if (doNotRegex.test(match[0])) {
+      matches.push({ match: match[0], regex: 'doNotRegex', index: match.index });
+    }
+  }
+  return matches;
+}
+
+const test = '-:-]what()(+/mul(957,396)?mul(550,844)%+why())-? #}from()mul(488,628)%} ~**mul(770,931)$~mul(791,733)<{mul(985,350)<#why()don\'t()what()select()$what())]what()who()mul(327,185))<^^mul(542,68)#?who()<from()`';
+
 const results = matches.map(multiply);
 const resultPart1 = results.reduce((accumulator, current) => accumulator + current, 0);
-console.log(resultPart1);
+console.log('Part1: ', resultPart1);
+
+const sortedMatches = findMatchesInOrder(allContents).sort((a,b) => a.index - b.index );
+let doTheThing = true;
+const part2Results = sortedMatches.reduce((acc, current) => {
+    if(current.regex === 'mulRegex' && doTheThing) {
+      acc = acc + multiply(current.match);
+    } else if(current.regex === 'doRegex') {
+      doTheThing = true;
+    } else if(current.regex === 'doNotRegex') {
+      doTheThing = false;
+    }
+    return acc;
+}, 0);
+
+console.log('Part2: ', part2Results);
